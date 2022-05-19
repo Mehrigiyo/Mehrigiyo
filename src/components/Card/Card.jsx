2e44579a7f75c58414b6540d18cca6db16c6fda8
shuhratcoderjs2 <shuhratcoderjs@gmail.com>
2022-05-19 20:32:41 +0500

    test3

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 export const Card = () => {
   return (
-    <div>Card  new post knrob\jrwov,
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam rem eveniet laboriosam! Quia ipsum ducimus veritatis 
+      quasi reprehenderit, neque, nihil qui labore sed ipsam aliquid quisquam officia? Accusamus, nisi reprehenderit!
+      Card  new post knrob\jrwov,
       <button>fpnmbks</button>
     </div>
   )
