4986a35052563bb0089baa1ee9a80e4f6da1cf46
Xojiakbar <aripovxojiakbar@gmail.com>
2022-05-19 12:51:37 +0300

    create pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import Home from "./pages/Home/Home";
+
 function App() {
-  return <div className="App">APP</div>;
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </Layout>
+  )
 }
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>Headerqqqqqqqqqqqqqqqqqqqqqqqqqq</div>
+  )
+}
+
+export default Header
