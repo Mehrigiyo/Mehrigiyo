3d6be83e02eac99f7a184e498733cf042a3065e6
Xojiakbar <aripovxojiakbar@gmail.com>
2022-05-21 23:17:48 +0300

    update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Row } from 'react-bootstrap'
+import "./Footer.css"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+    <div className='Footer globalContainer'>
+      <div className='foterRight'>
+       <p>© 1996-2021. Mehrigiyo. Barcha huquqlar himoyalangan. </p>
+       <p> Saytdan olingan barcha maʼlumotlar chop etilganda veb-saytga havola qilish majburiy. </p>
+      </div>
+      <div className='foterLeft'>
+        <h3>Bosh sahifa</h3>
+        <h3>Biz haqimizda</h3>
+        <h3>Yangiliklar</h3>
+      </div>
+    </div>
+    </>
   )
 }
 
