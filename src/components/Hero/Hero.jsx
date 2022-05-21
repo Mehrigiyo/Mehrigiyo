1bb7046283181ed2e9d5909d7a5df437e6e321ad
shuhratcoderjs3 <shuhratcoderjs2002@gmail.com>
2022-05-21 23:42:37 +0500

    hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 
+
+import './Hero.css'
 export default function Hero() {
   return (
-    <div>Hero</div>
+    <>
+    {/* <div className='hero-image'></div> */}
+        
+  <div class="hero-image">
+    <div class="hero-text">
+      <h1 style={{fontSize: 50}}>I am John Doe</h1>
+      <p>And I'm a Photographer</p>
+      <button>Hire me</button>
+    </div>
+  </div>
+    </>
   )
 }
