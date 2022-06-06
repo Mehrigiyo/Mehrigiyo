f292d23da207405d98879cfa3cf64a313abcb4e7
shuhratcoderjs3 <shuhratcoderjs2002@gmail.com>
2022-06-06 14:21:24 +0500

    da

--- src/App.js
+++ src/App.js
@@ -12,59 +12,6 @@ import Productlist from "./pages/Product/_components/Productlist/Productlist";
 import ProductAbout from "./pages/Product/_components/ProductAbout/ProductAbout";
 import LoveDoctor from "./pages/LoveDoctors/LoveDoctors";
 import LoveMedice from "./pages/LoveMedice/LoveMedice";
-<<<<<<< HEAD
-
-function App() {
-    return ( <
-        Context >
-        <
-        Layout >
-        <
-        Routes >
-        <
-        Route path = "/"
-        element = { < Home / > }
-        /> <
-        Route path = "/onlinedoctor"
-        element = { < OnlineDoctor / > }
-        /> <
-        Route path = "/product"
-        element = { < Product / > }
-        />  <
-        Route path = "/help"
-        element = { < Help / > }
-        />  <
-        Route path = "/aboute"
-        element = { < About / > }
-        />  <
-        Route path = "/askquestion"
-        element = { < AskQuestion / > }
-        /> <
-        Route path = "/doctorslist"
-        element = { < DoctorsList / > }
-        />  <
-        Route path = "/productlist"
-        element = { < Productlist / > }
-        />  <
-        Route path = "/productabout"
-        element = { < ProductAbout / > }
-        />  <
-        Route path = "/lovedoctors"
-        element = { < LoveDoctor / > }
-        />  <
-        Route path = "/lovemedice"
-        element = { < LoveMedice / > }
-        />  <
-        Route path = "/news"
-        element = { < NewsComponent / > }
-        /> < /
-        Routes > <
-        /Layout> < /
-        Context >
-
-
-    )
-=======
 import DoctorInfo from "./pages/OnlineDoctor/_components/DoctorInfo/DoctoInfo";
 import DoctorBron from "./pages/OnlineDoctor/_components/DoctorBron/DoctorBron";
 import News from "./pages/News/News";
@@ -91,7 +38,6 @@ function App() {
       </Layout>{" "}
     </Context>
   )
->>>>>>> 868a68d245a1ddc4973ea6942871bf7279908f34
 }
 
 export default App
