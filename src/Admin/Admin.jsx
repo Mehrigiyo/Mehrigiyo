624d555c07adeaa47e089263c8db032d741629b6
shuhratcoderjs3 <shuhratcoderjs2002@gmail.com>
2022-06-10 16:55:50 +0500

    gga

--- src/Admin/Admin.jsx
+++ src/Admin/Admin.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Header from '../User/components/Header/Header'
 import AdminLayout from './components/AdminLayout/AdminLayout'
-
+import Managementpanel from './pages/Managementpanel/Managementpanel';
+import Consultation from './pages/Consultation/Consultation';
+import DeliveryAddress from './pages/DeliveryAddress/DeliveryAddress';
+import PaymentMethods from './pages/PaymentMethods/PaymentMethods';
+import Orders from './pages/Orders/Orders';
+import Notifications from './pages/Notifications/Notifications';
 function Admin() {
   return (
     <div>
         {/* <AdminLayout/> */}
         <Header/>
+
     </div>
   )
 }
