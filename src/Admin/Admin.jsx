2a508f0ef654f3cd10856a6e4becf55cdf4f9dea
Navruz Zubaydullayev <zubaydullayevnavruz@gmail.com>
2022-06-11 12:20:17 +0500

    sidebar

--- src/Admin/Admin.jsx
+++ src/Admin/Admin.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import Header from "../User/components/Header/Header";
 import AdminLayout from "./components/AdminLayout/AdminLayout";
-
+import Consultation from './pages/Consultation/Consultation'
 function Admin() {
   return (
     <div>
       <AdminLayout>
-        {/* "routrla" */}
+        <Routes>
+          {/* <Route path="/" element={<Home />} />{" "} */}
+          <Route path="/consultation" element={<Consultation />} />{" "}
+        </Routes>
       </AdminLayout>
     </div>
   );
