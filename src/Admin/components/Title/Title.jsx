14ca8054ff0bfb76daf4d8347b277a83a7c9d2a9
shuhratcoderjs3 <shuhratcoderjs2002@gmail.com>
2022-06-12 15:05:23 +0500

    jhj

---
+++ src/Admin/components/Title/Title.jsx
@@ -0,0 +1,14 @@
+import './Title.scss'
+
+function Title() {
+  return (
+    
+    <>
+     Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Ratione quaerat veritatis, alias corporis necessitatibus omnis ullam 
+     eius animi illum adipisci mollitia assumenda. Voluptate illum ex aspernatur accusamus! Enim, veniam expedita.
+    </>
+  )
+}
+
+export default Title
--- src/Admin/pages/Managementpanel/Managementpanel.jsx
+++ src/Admin/pages/Managementpanel/Managementpanel.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import './Managementpanel.scss'
 
 function Managementpanel() {
   return (
