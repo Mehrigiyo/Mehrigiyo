c17e4637b9a67b7b6fbb0eadf595811227d2f9b5
shuhratcoderjs3 <shuhratcoderjs2002@gmail.com>
2022-06-17 08:46:06 +0500

    settigns

---
+++ src/Admin/pages/Settings/_components/SVG/Planeta.jsx
@@ -0,0 +1,17 @@
+const Planeta = (props) => (
+    <svg
+      width={14}
+      height={14}
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M1.375 4.75h11.25M7 13.188A6.188 6.188 0 1 0 7 .812a6.188 6.188 0 0 0 0 12.376Zm0 0c1.688 0 2.25-2.813 2.25-6.188C9.25 3.625 8.687.812 7 .812 5.312.813 4.75 3.626 4.75 7c0 3.375.563 6.188 2.25 6.188ZM1.375 9.25h11.25-11.25Z"
+        stroke="#5B7FFF"
+        strokeWidth={1.5}
+      />
+    </svg>
+  )
+  
+  export default Planeta
